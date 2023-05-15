dd1c3c387497e7b6ebef921eed909f9e7dd58745
hlav22 <hgl0006@mix.wvu.edu>
2023-05-15 13:13:00 -0400

    Panda Config for Output

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import numpy as np
+
+#Panda Config to Print all the data into the console
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_rows', 3000)
+
 
 # Reading the CSV file
-csvFile = pd.read_csv("S2122-laliga-players.csv", header=0, usecols=["competition", "name", "position"])
+csvFile = pd.read_csv('S2122-laliga-players.csv', header=0, usecols=['competition', 'name', 'team', 'position'])
 print(csvFile)
