fdd8a7d2b1b61cd665aa2b7599ac6b64ef87f4f0
hlav22 <hgl0006@mix.wvu.edu>
2023-05-15 20:15:01 -0400

    Added files for each position in LaLiga. Still need to implement functions for them.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import pandas as pd
+import laliga_santander
 import laliga_santander_forward
+import laliga_santander_midfielder
+import laliga_santander_defender
+import laliga_santander_goalkeeper
 
 
 def main():
     csv_file = 'S2122-laliga-santander.csv'
-    # print_santander(csv_file)
-    laliga_santander_forward.print_santander_forward(csv_file)
+    # laliga_santander.print_santander(csv_file)    #Prints all of LaLiga Santander statistics unformatted
+    laliga_santander_forward.print_santander_forward(
+        csv_file)  # Prints all LaLiga Santander forward statistics formatted
 
 
 if __name__ == '__main__':
